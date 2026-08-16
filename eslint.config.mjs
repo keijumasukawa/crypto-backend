b7b3a108a0d1f9aa7fe385e694b98ddafdf41328
Keiju Masukawa <keiju.masukawa@gmail.com>
2026-08-17 04:24:59 +0900

    chore: 検査設定の調整

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
-export default defineConfig(globalIgnores(["**/dist/"]), {
+export default defineConfig(globalIgnores(["**/dist/", "**/generated/"]), {
   files: ["**/*.{js,ts}"],
   extends: [
     js.configs.recommended,
